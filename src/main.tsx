98a6c9c93adb889396a93d3d913c1b029b3faee5
KimMH853 <kimmh853@gmail.com>
2023-05-07 15:30:12 +0900

    server 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetUser {
+        GetUser {
+          users {
+            id
+            email
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
